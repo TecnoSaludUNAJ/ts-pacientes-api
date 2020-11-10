bcaddffa2e7f72bec6c1100f50d168c584556f46
julinuzzo19 <julinuzzo19@gmail.com>
2020-11-10 20:39:35 -0300

    ports and connectionstring fixed

---
+++ TP-AccessData/Migrations/20201110233234_ts-pacientes-api.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TP_AccessData.Migrations
+{
+    public partial class tspacientesapi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
